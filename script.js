d4d86ad5b7ba0a5b689ac90fa229478ae35ba00a
napipuk <napipukkrawpan@gmail.com>
2026-06-12 15:01:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
+// Animation Loading
+const cards = document.querySelectorAll(".card");
 
-document.addEventListener('DOMContentLoaded', function () { 
-console.log('Portfolio ของ SOMCHAI พร้อมแล้ว!');
+window.addEventListener("load", () => {
+    cards.forEach((card, index) => {
+        card.classList.add("fade");
 
+        setTimeout(() => {
+            card.classList.add("show");
+        }, index * 250);
+    });
+});
+
+// Resume Button
+const resumeBtn = document.getElementById("resumeBtn");
+
+resumeBtn.addEventListener("click", () => {
+    alert("Resume Download Coming Soon!");
+});
+
+// Change Name Effect
+const nameText = document.getElementById("napipuk");
+
+nameText.addEventListener("mouseover", () => {
+    nameText.textContent = "WELCOME TO MY PROFILE";
+});
+
+nameText.addEventListener("mouseout", () => {
+    nameText.textContent = "napipuk";
 });
